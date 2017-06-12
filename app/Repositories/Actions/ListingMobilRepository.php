0752e73ea432ab5ae0ed49f26497e002e43402a4
thel5coder <my.ant2008@gmail.com>
2017-06-12 15:50:51 +0700

    contoh pagination repository

--- app/Repositories/Actions/ListingMobilRepository.php
+++ app/Repositories/Actions/ListingMobilRepository.php
@@ -79,7 +79,54 @@ class ListingMobilRepository implements IListingMobilRepository
 
     public function paginationData(PaginationParam $param)
     {
-        // TODO: Implement paginationData() method.
+        $result = new PaginationResult();
+
+        $order = $param->getSortOrder();
+        $orderBy = $param->getSortBy();
+
+        $sortBy = (isset($orderBy) ? 'id' : $orderBy);
+        $sortOrder = ($order == 'asc' ? 'desc' : $order);
+
+        //setup skip data for paging
+        if ($param->getPageSize() == -1) {
+            $skipCount = 0;
+        } else {
+            $skipCount = ($param->getPageIndex() * $param->getPageSize());
+        }
+        //get total count data
+        $result->setTotalCount(PjKategoriModel::count());
+
+        //get data
+        if ($param->getKeyword() == '') {
+
+            if ($skipCount == 0) {
+                $data = PjKategoriModel::take($param->getPageSize())
+                    ->orderBy($sortBy, $sortOrder)
+                    ->get();
+            } else {
+                $data = PjKategoriModel::skip($skipCount)->take($param->getPageSize())
+                    ->orderBy($sortBy, $sortOrder)
+                    ->get();
+            }
+        } else {
+            if ($skipCount == 0) {
+                $data = PjKategoriModel::where('nama_kategori', 'like', '%' . $param->getKeyword() . '%')
+                    ->take($param->getPageSize())
+                    ->orderBy($sortBy, $sortOrder)
+                    ->get();
+            } else {
+                $data =  PjKategoriModel::where('nama_kategori', 'like', '%' . $param->getKeyword() . '%')
+                    ->orderBy($sortBy, $sortOrder)
+                    ->skip($skipCount)->take($param->getPageSize())
+                    ->get();
+            }
+        }
+
+        $result->setCurrentPageIndex($param->getPageIndex());
+        $result->setCurrentPageSize($param->getPageSize());
+        $result->setResult($data);
+
+        return $result;
     }
 
     public function showByUserId($userId)
