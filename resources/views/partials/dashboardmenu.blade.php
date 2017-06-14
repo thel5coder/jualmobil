bea018db2c04185a150277910868a6f330df765d
thel5coder <my.ant2008@gmail.com>
2017-06-14 21:33:34 +0700

    f

--- resources/views/partials/dashboardmenu.blade.php
+++ resources/views/partials/dashboardmenu.blade.php
@@ -3,10 +3,10 @@
     <li><a href="{{route('formbuatiklan')}}">Buat Iklan Baru</a></li>
     <li><a href="{{route('berita')}}">Daftar Berita</a></li>
     <li><a href="{{route('formBerita')}}">Buat Berita Baru</a></li>
-@if(auth()->user()->tipe_user == 'admin')
+    @if(auth()->user()->tipe_user == 'admin')
         <li><a href="{{route('formMerk')}}">Tambah Merk Baru</a></li>
         <li><a href="{{route('formModel')}}">Tambah Model Baru</a></li>
-        <li ><a href="{{route('formTipe')}}">Tambah Tipe Baru</a></li>
+        <li><a href="{{route('formTipe')}}">Tambah Tipe Baru</a></li>
     @endif
     <li><a href="{{route('dashboard')}}">Akun Saya</a></li>
     <li><a href="{{route('logout')}}">Logout</a></li>
